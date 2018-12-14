6775e9d710e74cde3fb78f07070eeb99e247fe75
gejunxiang <gejunxiang@sogou-inc.com>
2018-12-14 11:38:36 +0800

    Change to char* struct, not use string

--- flash_sort/main.cpp
+++ flash_sort/main.cpp
@@ -27,7 +27,7 @@ typedef vector<char*> vtc;
 
 // global variable
 clock_t st = clock();
-vts bfs[LARGE];
+vtc bfs;
 char* bf;
 ll sz;
 
@@ -39,25 +39,32 @@ ll sz;
 //  }
 //  if (!cs.empty()) bfs.pb(cs);
 //}
+//
 
-//char* write_string_to_chs() {
-//  string ans;
-//  for (auto& it: bfs) {
-//    ans += it;
-//  }
-//  return const_cast<char*>(ans.c_str());
-//}
+int cs_len(char* s) {
+  for (int i = 0; ; ++i) {
+    if (s[i] == '\n') return i + 1;
+  }
+  return 0;
+}
 
-void write_string_to_chs() {
-  ll bias = 0;
-  bf = new char[sz];
-  for (auto& i: bfs) {
-    for (auto& j: i) {
-      sprintf(bf + bias, "%s\n", j.c_str());
-      bias += j.length() + 1;
-    }
+char* write_string_to_chs(FILE* f) {
+  string ans;
+  for (auto& it: bfs) {
+    fwrite(it, sizeof(char), cs_len(it), f);
   }
 }
+
+//void write_string_to_chs() {
+//  ll bias = 0;
+//  bf = new char[sz];
+//  for (auto& i: bfs) {
+//    for (auto& j: i) {
+//      sprintf(bf + bias, "%s\n", j.c_str());
+//      bias += j.length() + 1;
+//    }
+//  }
+//}
 
 string join_path(string l, string r) {
   if (l[l.length() - 1] == '/') return l + r;
@@ -84,39 +91,47 @@ int get_bucket_no(char* cs) {
   return res;
 }
 
-void track_bucket() {
-  ll bef = 0, cur = 0;
-  int rank = 0;
-  char cs[COM];
-  stringstream ifs(bf);
-  while (ifs >> cs) {
-    cur += strlen(cs);
-    rank = get_bucket_no(cs);
-    bfs[rank].pb(string(cs));
+//void track_bucket() {
+//  ll bef = 0, cur = 0;
+//  int rank = 0;
+//  char cs[COM];
+//  stringstream ifs(bf);
+//  while (ifs >> cs) {
+//    cur += strlen(cs);
+//    rank = get_bucket_no(cs);
+//    bfs[rank].pb(string(cs));
+//  }
+//}
+
+void write_to_bfs() {
+  bfs.pb(bf);
+  for (int i = 0; i < sz; ++i) {
+    if (bf[i] == '\n') bfs.pb(bf + i + 1);
   }
+  bfs.pop_back();
 }
 
 void get_strings_from_file(string ifs) {
   read_file(ifs);
-  track_bucket();
+  write_to_bfs();
   if (TESTTIME) fprintf(stderr, "Get strings from file %f seconds pass in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
 }
 
 bool cmp_cs(char* a, char* b) {
   int i = 0;
-  for (; i < strlen(a) && i < strlen(b); ++i) {
+  for (; a[i] != '\n' && b[i] != '\n'; ++i) {
     if (a[i] < b[i]) return true;
     else if (b[i] < a[i]) return false;
   }
-  if (i == strlen(a)) return true;
+  if (a[i] == '\n') return true;
   return false;
 }
 
-void sort_bucket() {
-  for (int i = 0; i < LARGE; i++) {
-    sort(bfs[i].begin(), bfs[i].end());
-  }
-}
+//void sort_bucket() {
+//  for (int i = 0; i < LARGE; i++) {
+//    sort(bfs[i].begin(), bfs[i].end());
+//  }
+//}
 
 int main(int argc, char** argv) {
   if (TESTTIME) fprintf(stderr, "Begin test...\n");
@@ -130,16 +145,14 @@ int main(int argc, char** argv) {
   // get vector strings
   get_strings_from_file(ifs);
   // sort
-  sort_bucket();
+  sort(bfs.begin(), bfs.end(), cmp_cs);
   if (TESTTIME) fprintf(stderr, "Sort strings %f seconds pass in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
   // write back to chars
-  write_string_to_chs();
-  if (TESTTIME) fprintf(stderr, "Write strings %f seconds pass in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
-  // write chars to ofs
   FILE* off = fopen(ofs.c_str(), "wb");
-  fwrite(bf, sizeof(char), sz, off);
+  write_string_to_chs(off);
+  if (TESTTIME) fprintf(stderr, "Write strings %f seconds pass in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
   fclose(off);
-  if (TESTTIME) fprintf(stderr, "Process takes %f seconds in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
   delete[] bf;
+  if (TESTTIME) fprintf(stderr, "Process takes %f seconds in total.\n", (float)(clock() - st) / CLOCKS_PER_SEC);
   return 0;
 }
